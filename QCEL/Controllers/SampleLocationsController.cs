c06e9d710d8be03b75a55f169d922d4e4c75adc2
Jeffrey <jeffreysantana26@gmail.com>
2019-05-29 12:34:35 -0500

    Created controller for sample locations

---
+++ QCEL/Controllers/SampleLocationsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QCEL.Controllers
+{
+    public class SampleLocationsController : Controller
+    {
+        // GET: SampleLocations
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: SampleLocations/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SampleLocations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // GET: SampleLocations/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+
+        // GET: SampleLocations/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+    }
+}
