c4d1c08918e60d215ff9a370e65dd6f6d01cecf3
Jeffrey <jeffreysantana26@gmail.com>
2019-05-29 13:06:55 -0500

    Added dummy data to display in view

--- QCEL/Controllers/SampleLocationsController.cs
+++ QCEL/Controllers/SampleLocationsController.cs
@@ -3,42 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QCEL.Models;
+using QCEL.ViewModels;
 
 namespace QCEL.Controllers
 {
-    public class SampleLocationsController : Controller
-    {
-        // GET: SampleLocations
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class SampleLocationsController : Controller
+	{
+		// GET: SampleLocations
+		public ActionResult Index()
+		{
+			var sampleLocations = new List<SampleLocation>()
+			{
+				new SampleLocation(){
+					Id = 1,
+					SampleNumber = "BL1",
+					Location = "BL1 Wash area drain",
+					MicroTest = "Listeria SP",
+					ProductCode = "Sponge",
+					RequestType = "Environmental",
+					Type = "Sponge Sample/1",
+					Zone = "3"
+				},
+				new SampleLocation(){
+					Id = 2,
+					SampleNumber = "BL2",
+					Location = "BL1 Blender Floor",
+					MicroTest = "Listeria SP",
+					ProductCode = "Sponge",
+					RequestType = "Environmental",
+					Type = "Sponge Sample/1",
+					Zone = "3"
+				}
+			};
 
-        // GET: SampleLocations/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+			var viewModel = new SampleLocationsViewModel
+			{
+				SampleLocations = sampleLocations
+			};
 
-        // GET: SampleLocations/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+			return View(viewModel);
+		}
+
+		// GET: SampleLocations/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: SampleLocations/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
 
 
-        // GET: SampleLocations/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+		// GET: SampleLocations/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
 
 
-        // GET: SampleLocations/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+		// GET: SampleLocations/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
 
-    }
+	}
 }
