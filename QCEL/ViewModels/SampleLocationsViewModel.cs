d3a78f55e56bb5bf8f8f350d52774d6d88f76926
Jeffrey <jeffreysantana26@gmail.com>
2019-05-29 13:08:07 -0500

    Added viewmodel to hold multiple sample locations

---
+++ QCEL/ViewModels/SampleLocationsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QCEL.Models;
+
+namespace QCEL.ViewModels
+{
+	public class SampleLocationsViewModel
+	{
+		public List<SampleLocation> SampleLocations { get; set; }
+	}
+}
