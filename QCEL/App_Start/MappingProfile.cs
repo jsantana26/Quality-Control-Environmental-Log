4717782ebc90c19a782d9c84001ef14e9d86189a
Jeffrey <jeffreysantana26@gmail.com>
2019-05-31 18:09:43 -0500

    Added configuration for automapper

---
+++ QCEL/App_Start/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using QCEL.Dtos;
+using QCEL.Models;
+
+namespace QCEL.App_Start
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			Mapper.CreateMap<SampleLocation, SampleLocationDto>();
+			Mapper.CreateMap<SampleLocationDto, SampleLocation>()
+				.ForMember(c => c.Id, opt => opt.Ignore());
+		}
+	}
+}
