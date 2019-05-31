92aaa2a24e7506b81690b45e8aab7f3792ba664c
Jeffrey <jeffreysantana26@gmail.com>
2019-05-31 18:10:17 -0500

    Create Dto for data received from the api

---
+++ QCEL/Dtos/SampleLocationDto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QCEL.Dtos
+{
+	public class SampleLocationDto
+	{
+		public int Id { get; set; }
+		/// <summary>
+		/// Sample number for test
+		/// </summary>
+		public string SampleNumber { get; set; }
+
+		/// <summary>
+		/// Location of which area was tested
+		/// </summary>
+		public string Location { get; set; }
+
+		/// <summary>
+		/// Type of test performed for the sample
+		/// </summary>
+		public string MicroTest { get; set; }
+
+		/// <summary>
+		/// Type of sample that was taken
+		/// </summary>
+		public string Type { get; set; }
+
+		/// <summary>
+		/// Zone of location tested
+		/// </summary>
+		public string Zone { get; set; }
+
+		/// <summary>
+		/// Product code for the instrument used for the testing
+		/// </summary>
+		public string ProductCode { get; set; }
+
+		/// <summary>
+		/// Type of test that was taken
+		/// </summary>
+		public string RequestType { get; set; }
+	}
+}
