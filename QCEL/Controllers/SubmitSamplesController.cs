20eb5f0f33d3a2c36c6fcdaae0186598ae28a0d5
Jeffrey <jeffreysantana26@gmail.com>
2019-06-09 15:40:17 -0500

    Created a controller and index view for submit samples

---
+++ QCEL/Controllers/SubmitSamplesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QCEL.Controllers
+{
+    public class SubmitSamplesController : Controller
+    {
+        // GET: SubmitSamples
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
