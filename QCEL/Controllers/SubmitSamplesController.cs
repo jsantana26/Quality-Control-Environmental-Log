6dbdcc42d7bd874ba5b61e0faa4c91323f13e915
Jeffrey <jeffreysantana26@gmail.com>
2019-06-09 15:48:40 -0500

    Pass environmental samples that still have pending submissions
    Get all samples from the db where pending samples is true and pass those to the view

--- QCEL/Controllers/SubmitSamplesController.cs
+++ QCEL/Controllers/SubmitSamplesController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QCEL.Models;
 
 namespace QCEL.Controllers
 {
     public class SubmitSamplesController : Controller
     {
+	    private ApplicationDbContext _context;
+
+	    public SubmitSamplesController()
+	    {
+		    _context = new ApplicationDbContext();
+	    }
+
         // GET: SubmitSamples
         public ActionResult Index()
         {
-            return View();
+	        var environmentalSample = _context.EnvironmentalSamples.Where(c => c.PendingSubmission).ToList();
+            return View(environmentalSample);
         }
     }
 }
