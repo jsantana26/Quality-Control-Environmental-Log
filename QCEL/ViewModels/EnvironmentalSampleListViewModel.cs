c31dde4d05eaf7c3dffc5f6cf01169f3cac695fa
Jeffrey <jeffreysantana26@gmail.com>
2019-06-09 19:43:21 -0500

    Remaned property to SelectedToSubmit
    This property represents samples that are selected and ready to submit in the SARF form

---
+++ QCEL/ViewModels/EnvironmentalSampleListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QCEL.Models;
+
+namespace QCEL.ViewModels
+{
+	public class EnvironmentalSampleListViewModel
+	{
+		public List<EnvironmentalSample> SelectedToSubmit;
+	}
+}
