13b3b02fee248cb67be7cefd86fab2dd087f6b43
Jeffrey <jeffreysantana26@gmail.com>
2019-06-16 17:41:46 -0500

    Added route to the api getDescription call

--- QCEL/App_Start/WebApiConfig.cs
+++ QCEL/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
@@ -18,6 +19,13 @@ namespace QCEL
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
+				name: "GetDescriptionApi",
+				routeTemplate: "api/{controller}/{location}",
+				new { controller = "SampleLocations", action = "GetDescription", id = UrlParameter.Optional }
+            );
+
+
+			config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
