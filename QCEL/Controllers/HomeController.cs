e2a5b4f93f23b49d74e1074b80fa92030b05d827
Jeffrey <jeffreysantana26@gmail.com>
2019-06-15 19:39:33 -0500

    Added button on home page to navigate to print labels controller

--- QCEL/Controllers/HomeController.cs
+++ QCEL/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using QCEL.Models;
 
 namespace QCEL.Controllers
@@ -18,11 +19,19 @@ namespace QCEL.Controllers
 
 		public ActionResult Index()
 		{
+			var userId = User.Identity.GetUserId();
+			var currentUser = _context.Users.FirstOrDefault(x => x.Id == userId);
+			var userInitials = currentUser.FirstName[0].ToString() + currentUser.LastName[0].ToString();
+
 			var todaysSamples = _context.EnvironmentalSamples.Count(c => c.CollectionDate == DateTime.Today);
 			var pendingSamples = _context.EnvironmentalSamples.Count(c => c.Submitted == false);
+			var labelsReady = _context.EnvironmentalSamples
+				.Where(c => c.LabelPrinted == false)
+				.Count(c => c.Initials == userInitials);
 
 			ViewBag.TodaysSamples = todaysSamples;
 			ViewBag.PendingSamples = pendingSamples;
+			ViewBag.LabelsReady = labelsReady;
 
 			if(User.IsInRole(RoleName.CanManageAccounts) || User.IsInRole(RoleName.CanManageSampleLocations))
 				return View("AdminIndex");
