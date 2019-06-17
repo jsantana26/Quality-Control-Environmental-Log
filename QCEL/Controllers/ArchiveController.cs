a1cecffc23ce624180ecf980a571a67047ff6ded
Jeffrey <jeffreysantana26@gmail.com>
2019-06-17 14:25:45 -0500

    Added daterange filtering on archive controller Users will be allowed to select a date range of the samples they want to see

--- QCEL/Controllers/ArchiveController.cs
+++ QCEL/Controllers/ArchiveController.cs
@@ -3,15 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QCEL.Models;
+using QCEL.ViewModels;
 
 namespace QCEL.Controllers
 {
     public class ArchiveController : Controller
     {
+	    private ApplicationDbContext _context;
+
+	    public ArchiveController()
+	    {
+		    _context = new ApplicationDbContext();
+	    }
         // GET: Archive
         public ActionResult Index()
         {
-            return View();
+	        var viewModel = new DateRangeViewModel
+	        {
+		        EnvironmentalSamples = _context.EnvironmentalSamples.ToList()
+	        };
+            return View(viewModel);
+        }
+
+        public ActionResult FilterDate(DateRangeViewModel viewModel)
+        {
+	        if (!ModelState.IsValid)
+		        return RedirectToAction("Index");
+
+	        if (viewModel.FromDate == null)
+				viewModel.FromDate = DateTime.Today;
+
+			if(viewModel.ToDate == null)
+				viewModel.ToDate = DateTime.Today;
+
+			viewModel.ToDate = new DateTime(viewModel.ToDate.Value.Year, viewModel.ToDate.Value.Month, DateTime.DaysInMonth(viewModel.ToDate.Value.Year, viewModel.ToDate.Value.Month));
+
+	        viewModel.EnvironmentalSamples = _context.EnvironmentalSamples
+		        .Where(c => c.CollectionDate >= viewModel.FromDate && c.CollectionDate <= viewModel.ToDate)
+		        .ToList();
+
+	        return View("Index", viewModel);
         }
     }
 }
---
+++ QCEL/ViewModels/DateRangeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using QCEL.Models;
+
+namespace QCEL.ViewModels
+{
+	public class DateRangeViewModel
+	{
+		public List<EnvironmentalSample> EnvironmentalSamples { get; set; }
+		public DateTime? FromDate { get; set; }
+		public DateTime? ToDate { get; set; }
+	}
+}
